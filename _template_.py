c2525458d7cc015bca3e8747a66835febe96e663
Karolis Ramanauskas <karolisr@gmail.com>
2013-02-02 23:32:13 -0600

    Added _template_.py file. This file contains a standard block of code for new files.

---
+++ _template_.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+if __name__ == '__main__':
+    
+    # Tests
+    
+    import os
+
+    PS = os.path.sep
+
+    pass
