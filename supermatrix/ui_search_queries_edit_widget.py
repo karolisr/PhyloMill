849c9007b2987891b6e7613a4d7c63afb632d515
Karolis Ramanauskas <karolisr@gmail.com>
2013-12-27 22:18:30 -0600

    Added ui_search_queries_edit_widget.

---
+++ supermatrix/ui_search_queries_edit_widget.py
@@ -0,0 +1,26 @@
+'''
+Created on Dec 27, 2013
+@author: Karolis Ramanauskas
+@copyright: 2013 Karolis Ramanauskas. All rights reserved.
+@license: GPLv3
+'''
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+__updated__ = '2013-12-27'
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+if __name__ == '__main__':
+    pass
