24df1f36d5aa6022d23d0aaf88d9dfb8c05c14d0
Karolis Ramanauskas <karolisr@gmail.com>
2013-12-27 23:18:08 -0600

    Edited TreeNode class.

--- tree_node.py
+++ tree_node.py
@@ -1,6 +1,30 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on Dec 27, 2013
+@author: Karolis Ramanauskas
+@copyright: 2013 Karolis Ramanauskas. All rights reserved.
+@license: GPLv3
+'''
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+__all__ = []
+__version__ = 0.1
+__updated__ = '2013-12-27'
+
+
 class TreeNode(object):
+    '''
+    documentation
+    '''
 
     def __init__(self, name, parent=None):
+        '''
+        documentation
+        '''
 
         self._name = name
         self._children = []
@@ -13,6 +37,9 @@ class TreeNode(object):
 #         return "NODE"
 
     def add_child(self, child):
+        '''
+        documentation
+        '''
         self._children.append(child)
 
 #     def insertChild(self, position, child):
@@ -35,41 +62,66 @@ class TreeNode(object):
 #         return True
 
     def name(self):
+        '''
+        documentation
+        '''
         return self._name
 
     def set_name(self, name):
+        '''
+        documentation
+        '''
         self._name = name
 
     def child(self, row):
+        '''
+        documentation
+        '''
         return self._children[row]
 
     def children_count(self):
+        '''
+        documentation
+        '''
         return len(self._children)
 
     def parent(self):
+        '''
+        documentation
+        '''
         return self._parent
 
     def row(self):
+        '''
+        documentation
+        '''
         if self._parent is not None:
             return self._parent._children.index(self)
 
-    def log(self, tabLevel=-1):
+    def log(self, tab_level=-1):
+        '''
+        documentation
+        '''
 
         output = ""
-        tabLevel += 1
+        tab_level += 1
 
-        for i in range(tabLevel):
+        for current_tab_level in range(tab_level):
             output += "\t"
 
         output += "|------" + self._name + "\n"
 
         for child in self._children:
-            output += child.log(tabLevel)
+            output += child.log(tab_level)
 
-        tabLevel -= 1
+        tab_level -= 1
         output += "\n"
 
         return output
 
     def __repr__(self):
+
         return self.log()
+
+if __name__ == '__main__':
+    pass
