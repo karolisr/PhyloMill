6bc2ab9c4afd2f20c7d51ca3877fed172007ef52
Karolis Ramanauskas <karolisr@gmail.com>
2014-04-26 13:29:37 -0500

    Adds create project template feature.

--- workflows/phylo.py
+++ workflows/phylo.py
@@ -6,4 +6,45 @@ from __future__ import unicode_literals
 
 if __name__ == '__main__':
 
-    pass
+    import os
+    import sys
+    import argparse
+    import inspect
+    import shutil
+
+    ps = os.path.sep
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-p',
+        '--prepare_project_dir',
+        type=unicode,
+        help='Prepares clean project directory.')
+
+    args = parser.parse_args()
+
+    # Prepare clean project directory
+    if args.prepare_project_dir:
+
+        prj_dir_path = args.prepare_project_dir.rstrip(ps)
+
+        if os.path.exists(prj_dir_path):
+            print('Directory at', prj_dir_path, 'already exists.')
+            sys.exit(0)
+
+        prj_dir_path = prj_dir_path + ps
+
+        # Script filename
+        script_file_path = inspect.getfile(inspect.currentframe())
+        # Script directory path
+        script_dir_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+        prj_template_dir_path = script_dir_path.strip('workflows') + 'data' + ps + 'phylo-prj-tempate'
+
+        shutil.copytree(prj_template_dir_path, prj_dir_path, symlinks=False, ignore=None)
+        # wd = os.getcwd()
+        # os.chdir(prj_dir_path+'03-name_resolution')
+        # call(['./get_ncbi_data.sh'], stdout=open(os.devnull, 'wb'))
+        # os.chdir(wd)
+        sys.exit(0)
