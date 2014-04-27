2644ecf93aa5769d4de29807767e9fac2b94e707
Karolis Ramanauskas <karolisr@gmail.com>
2014-04-27 11:27:24 -0500

    Removes all old phylo workflow related files.

--- data/phylobuilder-project-template-old/03-name_resolution/get_ncbi_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget -q ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
-mkdir ncbi
-tar xzf taxdump.tar.gz -C ncbi
-rm -rf taxdump.tar.gz
-mv ncbi/names.dmp ncbi_tax_names
-rm -rf ncbi
