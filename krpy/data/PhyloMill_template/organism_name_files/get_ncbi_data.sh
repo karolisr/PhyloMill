9be4334565113a2b80cb0c6b4db8c76e47dfe8b9
Karolis Ramanauskas <karolisr@gmail.com>
2014-07-12 16:39:26 -0500

    Replaces wget with curl.

--- krpy/data/PhyloMill_template/organism_name_files/get_ncbi_data.sh
+++ krpy/data/PhyloMill_template/organism_name_files/get_ncbi_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
+curl -s -o taxdump.tar.gz ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 mkdir ncbi
 tar xzf taxdump.tar.gz -C ncbi
 rm -rf taxdump.tar.gz
