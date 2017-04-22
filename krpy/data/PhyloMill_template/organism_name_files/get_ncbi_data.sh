dfd9fc51296c68faafb2670cdf4e82684be4f9fc
Karolis Ramanauskas <karolisr@gmail.com>
2017-04-21 19:21:33 -0500

    New branch for major cleaning up.

--- krpy/data/PhyloMill_template/organism_name_files/get_ncbi_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s -o taxdump.tar.gz ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
-mkdir ncbi
-tar xzf taxdump.tar.gz -C ncbi
-rm -rf taxdump.tar.gz
-mv ncbi/names.dmp ncbi_tax_names
-rm -rf ncbi
